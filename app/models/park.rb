4758410434b993b8f18aa932e4dff87d89b26c8d
Nick Jacobs <nickjacobsatc@gmail.com>
2022-05-12 16:54:07 -0500

    Added validate presence of

--- app/models/park.rb
+++ app/models/park.rb
@@ -1,3 +1,6 @@
 class Park < ApplicationRecord
   has_many :trails
+  validates_presence_of :name
+  validates_presence_of :size
+  validates_inclusion_of :open, :in => [true,false]
 end
--- app/models/trail.rb
+++ app/models/trail.rb
@@ -1,3 +1,6 @@
 class Trail < ApplicationRecord
   belongs_to :park
+  validates_presence_of :name
+  validates_presence_of :length
+  validates_inclusion_of :hikable, :in => [true,false]
 end
