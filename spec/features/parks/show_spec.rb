a4a2b52e894f48b643aaced6fb6e5c6fe045afb2
Nick Jacobs <nickjacobsatc@gmail.com>
2022-05-12 16:57:03 -0500

    Added displays park attributes test

--- spec/features/parks/show_spec.rb
+++ spec/features/parks/show_spec.rb
@@ -1,8 +1,20 @@
 require 'rails_helper'
 
+# User Story 2, Parent Show
+# As a visitor
+# When I visit '/parents/:id'
+# Then I see the parent with that id including the parent's attributes:
+# - data from each column that is on the parent table
+
 RSpec.describe "the parks show page" do
   it "displays park attributes" do
-    park = Park.create(name:'Yosemite', open:true, size:1187)
-    visit "/parks/#{park.id}"
+    yosemite = Park.create!(name:'Yosemite', open:true, size:1169)
+    grand_canyon = Park.create!(name:'Grand Canyon', open:true, size:1902)
+    visit "/parks/#{yosemite.id}"
+
+    expect(page).to have_content(yosemite.name)
+    expect(page).to have_content(yosemite.open)
+    expect(page).to have_content(yosemite.size)
+    expect(page).to_not have_content(grand_canyon.name)
   end
 end
