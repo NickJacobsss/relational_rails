677da3d4dec18156c7f087e25f092ef565710bd9
Nick Jacobs <nickjacobsatc@gmail.com>
2022-05-15 13:25:41 -0500

    Feat: added route to parks#new

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/parks', to: 'parks#index'
+  get '/parks/new', to: 'parks#new'
   get '/parks/:id', to: 'parks#show'
   get '/trails', to: 'trails#index'
   get '/trails/:id', to: 'trails#show'
   get '/parks/:id/trails', to: 'park_trails#index'
+  post '/parks', to: 'parks#create'
+
 end
