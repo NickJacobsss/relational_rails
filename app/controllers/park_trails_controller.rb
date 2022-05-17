db03e640f18ac35bea48fb540bfeab3522371176
Nick Jacobs <nickjacobsatc@gmail.com>
2022-05-16 19:11:24 -0500

    Feat: New and create methods

--- app/controllers/park_trails_controller.rb
+++ app/controllers/park_trails_controller.rb
@@ -1,6 +1,22 @@
 class ParkTrailsController < ApplicationController
   def index
     @park = Park.find(params[:id])
+    @trail = Trail.all
     @trails = @park.trails
+  end
+
+  def new
+    @park = Park.find(params[:id])
+  end
+
+  def create
+    @park = Park.find(params[:id])
+    new_trail = @park.trails.create(park_trail_params)
+    redirect_to "/parks/#{@park.id}/trails"
+  end
+
+  private
+  def park_trail_params
+    params.permit(:name, :hikable, :length)
   end
 end
